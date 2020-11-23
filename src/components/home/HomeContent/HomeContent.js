b23d8b49e6824b851dd55cf30732a040b09c076f
DorianStanciu <conturi_dorian2@yahoo.com>
2020-11-23 23:38:30 +0000

    23/11/2020

--- src/components/home/HomeContent/HomeContent.js
+++ src/components/home/HomeContent/HomeContent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './HomeContent.css';
 import Movies from '../../search-results/Movies';
+import Latest from './latest/Latest';
 
 class HomeContent extends Component {
     render() {
         return (
             <div className='homecontent'>
                 <Movies />
+                <Latest />
             </div>
         )
     }
