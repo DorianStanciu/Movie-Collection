6a7802c1ee28d49b31a9caeb195515b0bdcb338b
DorianStanciu <conturi_dorian2@yahoo.com>
2020-12-02 23:58:03 +0000

    2/12/2020

---
+++ src/components/explore/Explore.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Explore.css';
+
+export class Explore extends Component {
+    render() {
+        return (
+            <div>
+                explore
+            </div>
+        )
+    }
+}
+
+export default Explore;
+
